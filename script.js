3da6d8f0a610a7ba1cf9301155629bf1b7868c5b
Jaskirat Singh <jsmokha33@gmail.com>
2022-08-15 22:51:10 +0530

    FAQ section updated

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const navbarLinks = document.getElementsByClassName('nav-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach( button =>{
+    button.addEventListener('click',()=>{
+        const faq = button.nextElementSibling;
+        const icon = button.children[1];
+
+        faq.classList.toggle('show');
+        icon.classList.toggle('rotate');
+    })
+} )
